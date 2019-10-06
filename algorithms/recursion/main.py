09a684742356e6edb041bf378fcf8ded60f77ca8
Влад Летун <fyks1649@gmail.com>
2019-10-06 13:09:11 +0300

    recursion

--- algorithms/recursion/main.py
+++ algorithms/recursion/main.py
@@ -1,3 +1,8 @@
+# Required recursion conditions:
+# 1) recurrence case
+# 2) extreme case
+
+
 # simple recursion
 def counddown(i):
     print(i)
@@ -8,3 +13,15 @@ def counddown(i):
 
 
 counddown(10)
+
+# matryoshka
+def matryoshka(n):
+    if n == 1:
+        print("The smallest matryoshka")
+    else:
+        print("Top matryoshka n=", n)
+        matryoshka(n-1)
+        print("Down matryoshka n=", n)
+
+
+matryoshka(5)
